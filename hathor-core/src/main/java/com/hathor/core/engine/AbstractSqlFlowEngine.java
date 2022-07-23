b446a70cff5aff42771dada9f1825e37cd7229c6
Shkin1 <240493030@qq.com>
2022-07-24 00:42:11 +0800

    add sql engine

---
+++ hathor-core/src/main/java/com/hathor/core/engine/AbstractSqlFlowEngine.java
@@ -0,0 +1,41 @@
+package com.hathor.core.engine;
+
+/**
+ * <p>AbstractSqlFlowEngine</p>
+ * <p>Description:
+ * </p>
+ *
+ * @author 普帝
+ * @version v1.0.0
+ * @date 2022/07/2022/7/24 00:37
+ * <p>
+ * Modification History:
+ * Date         Author          Version            Description
+ * ------------------------------------------------------------
+ */
+public abstract class AbstractSqlFlowEngine implements ISqlFlow {
+
+
+    /**
+     * sql format.
+     *
+     * @param sqlStr old sql
+     * @return sql format
+     */
+    private String sqlFormat(String sqlStr){
+        // TODO.
+        return null;
+    }
+
+    /**
+     * sql rm desc.
+     *
+     * @param sqlStr old sql
+     * @return no desc sql
+     */
+    private String rmSqlDesc(String sqlStr) {
+        // TODO.
+        return null;
+    }
+
+}
---
+++ hathor-core/src/main/java/com/hathor/core/engine/ISqlFlow.java
@@ -0,0 +1,17 @@
+package com.hathor.core.engine;
+
+/**
+ * <p>ISqlFlow</p>
+ * <p>Description:
+ * </p>
+ *
+ * @author 普帝
+ * @version v1.0.0
+ * @date 2022/07/2022/7/24 00:38
+ * <p>
+ * Modification History:
+ * Date         Author          Version            Description
+ * ------------------------------------------------------------
+ */
+public interface ISqlFlow {
+}
